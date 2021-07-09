35f8401741b9afdd754f43fcf6e9249a817bc3db
Taimoorkhokhar94 <taimoorkhokhar1994@gmail.com>
2021-07-09 17:59:20 +0500

    Remove zerorpc function and create grpc server

--- example.py
+++ example.py
@@ -1,21 +1,4 @@
-import zerorpc
-PORT = 42422
 
-def handleMessage(sender, message):
-    print("Handle messages to respond to here!");
-    return "Respond to message from " + sender + " | " + message
-
-class PythonServer(object):
-    def listen(self):
-        print("Listning on"+str(PORT))
-    def message(self, sender, message):
-        return handleMessage(sender, message)
-
-try:
-    s = zerorpc.Server(PythonServer())
-    s.bind(f'tcp://0.0.0.0:{PORT}')
-    s.run()
-    print('PythonServer running...')
-except Exception as e:
-    print('unable to start PythonServer:', e)
-    raise e
+def handle_message(message):
+    print("Handle messages to respond to here!")
+    return "Respond to message | " + message
---
+++ grpc_server.py
@@ -0,0 +1,44 @@
+import grpc
+from concurrent import futures
+import time
+
+# import the generated classes
+import example_pb2
+import example_pb2_grpc
+
+# import the original example.py
+import example
+
+# create a class to define the server functions, derived from
+# example_pb2_grpc.AgentServicer
+class AgentServicer(example_pb2_grpc.AgentServicer):
+
+    # example.handle_message is exposed here
+    # the request and response are of the data type
+    # example_pb2.RequestMessage
+    def HandleMessage(self, request, context):
+        response = example_pb2.RequestMessage()
+        response.value = example.handle_message(request.value)
+        return response
+
+
+# create a gRPC server
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+# use the generated function `add_AgentServicer_to_server`
+# to add the defined class to the server
+example_pb2_grpc.add_AgentServicer_to_server(
+        AgentServicer(), server)
+
+# listen on port 50051
+print('Starting server. Listening on port 50051.')
+server.add_insecure_port('[::]:50051')
+server.start()
+
+# since server.start() will not block,
+# a sleep-loop is added to keep alive
+try:
+    while True:
+        time.sleep(86400)
+except KeyboardInterrupt:
+    server.stop(0)
