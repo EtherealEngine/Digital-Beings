645964bf0a453646ff52163eda6c64e994d3a552
Taimoorkhokhar94 <taimoorkhokhar1994@gmail.com>
2021-07-17 02:14:19 +0500

    handle grpc server using promise

--- src/pyconnect.ts
+++ src/pyconnect.ts
@@ -3,7 +3,7 @@ const path = require('path');
 const grpc = require('grpc');
 
 const proto = grpc.load(`${__dirname}/../example.proto`)
-const PORT = 50050
+const PORT = 50060
 const IP = 'localhost'
 
 class PyConnect {
@@ -11,26 +11,31 @@ class PyConnect {
     static grpcProcess: any;
     static grpc: any;
     static server() {
-        if (!PyConnect.connected) {
-            console.log('PythonConnector – making a new connection to the python layer');
-            PyConnect.grpcProcess = spawn('python3', ['-u', path.join(__dirname, '../grpc_server.py')]);
-            PyConnect.grpcProcess.stdout.on('data', function(data) {
-                console.info('python:', data.toString());
-            });
-            PyConnect.grpcProcess.stderr.on('data', function(data) {
-                console.error('python:', data.toString());
-            });
-            PyConnect.grpc = new proto.Agent(IP + ':' + PORT, grpc.credentials.createInsecure());
-            PyConnect.connected = true;
-        }
-        return PyConnect.grpc;
+        return new Promise((resolve, reject) => {
+            if (!PyConnect.connected) {
+                console.log('PythonConnector – making a new connection to the python layer');
+                PyConnect.grpcProcess = spawn('python3', ['-u', path.join(__dirname, '../grpc_server.py')]);
+                PyConnect.grpcProcess.stdout.on('data', function(data) {
+                    console.info('python:', data.toString());
+                    PyConnect.grpc = new proto.Agent(IP + ':' + PORT, grpc.credentials.createInsecure());
+                    PyConnect.connected = true;
+                    resolve(PyConnect.grpc);
+                });
+                PyConnect.grpcProcess.stderr.on('data', function(data) {
+                    console.error('python:', data.toString());
+                });
+            }
+            else{
+                resolve(PyConnect.grpc);
+            }
+        });
     }
 
     static async invoke(method, ...args) {
         try {
-            
-            var grpc = PyConnect.server();
-            return await promisify(grpc.HandleMessage, grpc, method, ...args);
+            return await PyConnect.server().then(async (grpc) => {
+                return await promisify(grpc, method, ...args);
+            });
         }
         catch (e) {
             return Promise.reject(e)
@@ -38,11 +43,8 @@ class PyConnect {
     }
 }
 
-var promisify = (fn, ctx, ...args) => {
-    if (!ctx) {
-        ctx = fn;
-    }
-
+var promisify = (ctx, ...args) => {
+    const fn = ctx.HandleMessage;
     return new Promise((resolve, reject) => {
         args.push((err, data) => {
             if (err) {
@@ -52,7 +54,6 @@ var promisify = (fn, ctx, ...args) => {
                 resolve(data);
             }
         });
-
         fn.apply(ctx, [args[0], args[1]])
     });
 };
