9be0547730285e48e87f7ed40082487a1265f87b
Taimoorkhokhar94 <taimoorkhokhar1994@gmail.com>
2021-07-18 02:34:12 +0500

    extend discord client to respond only when bot is mentioned

--- src/discord-client.ts
+++ src/discord-client.ts
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { Util } = require('discord.js')
 const DISCORD_API_TOKEN = process.env.DISCORD_API_TOKEN;
 const createDiscordClient = (messageResponseHandler) => {
   if(!DISCORD_API_TOKEN) return console.warn("No API token for Discord bot, skipping");
@@ -13,9 +14,25 @@ const createDiscordClient = (messageResponseHandler) => {
       if (message.author.bot)
         return; // Skip own messages
       if (message.channel.type === 'text') {
-        await messageResponseHandler(message.author.username, message.content, (response) => message.channel.send(response.value));
+        if (message.mentions.has(client.user)) {
+          message.channel.startTyping();
+          let removeMentions= message.content.split(" ").slice(1).join(" ")
+          message.content = removeMentions.replace(/<@[!&]?\d+>/, '[mention]');
+          await messageResponseHandler(message.author.username, message.content, (response) => {
+            message.channel.send(response.value)
+            message.channel.stopTyping();
+          });
+        }
       } else if (message.channel.type === 'dm') {
-        await messageResponseHandler(message.author.username, message.content, (response) => message.author.send(response.value));
+        if (message.mentions.has(client.user)) {
+          message.channel.startTyping();
+          let removeMentions= message.content.split(" ").slice(1).join(" ")
+          message.content = removeMentions.replace(/<@[!&]?\d+>/, '[mention]');
+          await messageResponseHandler(message.author.username, message.content, (response) => {
+            message.author.send(response.value)
+            message.channel.stopTyping();
+          });
+        }
       }
     });
   });
