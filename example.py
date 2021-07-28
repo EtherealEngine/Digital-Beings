d2db77e4ebe377f03a70db14616dcd5f32f6247f
Taimoorkhokhar94 <taimoorkhokhar1994@gmail.com>
2021-07-28 20:10:23 +0500

    change headers Authorization in gpt3 api call

--- example.py
+++ example.py
@@ -7,23 +7,26 @@ import os
 
 def handle_message(sender, message):
     print("Handle messages to respond to here!")
+    print(sender)
+    print(message)
     if sender and message:
-        engine = 'curie-instruct-beta'
-        context = 'Agent is a teacher expert in Web Development. You have skills in HTML And the problem your student is telling you about is I have been trying to center a div inside another div for over an hour now, I just can not do it, i need help with it..'
-        rasa_model_name = '1'
-        gpt3_agent = Gpt3Agent(engine, context, message)
-        rasa_agent = RasaAgent(rasa_model_name, message)
-        gpt3_response = gpt3_agent.invoke_api()
-        rasa_response = rasa_agent.invoke()
-        return "Respond to message from " + sender + " | " + message + "\n\n" + "GPT3 Response" + " | " + gpt3_response + "\n\n" + "Rasa Response" + " | " + rasa_response
-
+        try:
+            engine = 'curie-instruct-beta'
+            context = 'Agent is a teacher expert in Web Development. You have skills in HTML And the problem your student is telling you about is I have been trying to center a div inside another div for over an hour now, I just can not do it, i need help with it..'
+            rasa_model_name = '1'
+            gpt3_agent = Gpt3Agent(engine, context, message)
+            rasa_agent = RasaAgent(rasa_model_name, message)
+            gpt3_response = gpt3_agent.invoke_api()
+            rasa_response = rasa_agent.invoke()
+            return "Respond to message from " + sender + " | " + message + "\n\n" + "GPT3 Response" + " | " + gpt3_response + "\n\n" + "Rasa Response" + " | " + rasa_response
+        except Exception as err:
+            return str("Exception: ", err)
 
 class Gpt3Agent():
     engine_name = ''
     chat_context = ''
     question = ''
     agent_response = ''
-    
     
     def __init__(self, engine, context, question):
         self.engine_name = engine
@@ -42,21 +45,20 @@ class Gpt3Agent():
                                         max_tokens=80
                                     )
         self.agent_response = response.choices[0].text.strip()
-        
         return self.agent_response
     
     
     def invoke_api(self):
         url = f"https://api.openai.com/v1/engines/{self.engine_name}/completions"
         payload = "{\n\t \"prompt\": \"" + self.chat_context + "\\nQ:" + self.question + "\\nAgent:\",\n\t  \"max_tokens\": 25,\n\t  \"temperature\": 1,\n\t  \"top_p\": 1,\n\t  \"n\": 1,\n\t  \"stream\": false,\n\t  \"logprobs\": null,\n\t  \"stop\": \"\\n\"\n}"        
+        api_key = os.getenv("OPENAI_API_KEY")
         headers = {
             'content-type': "application/json",
-            'authorization': "Bearer sk-7c8vc7fXQhauFBXy8mXxtfX998p91iku8ktWWm4J"
+            'authorization': f"Bearer {api_key}"
             }
         response = requests.request("POST", url, data=payload, headers=headers)
         response_dic = json.loads(response.text)
         self.agent_response = response_dic['choices'][0]['text']
-
         return self.agent_response
 
 
@@ -89,9 +91,7 @@ class RasaAgent():
             self.response = loop.run_until_complete(agent.handle_text(self.question))
             agent_response_data = agent.tracker_store.retrieve_full_tracker("default")._latest_message_data()
             intent = agent_response_data.get('intent',{})
-            confidence = intent.get('confidence', 0)
-            print("rasa response => ", self.response[0].get("text"))
-            
+            confidence = intent.get('confidence', 0)            
             if confidence >= 0.20:
                 return self.response[0].get("text")
             else:
