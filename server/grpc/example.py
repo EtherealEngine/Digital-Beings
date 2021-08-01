f4432c5574456bafc901c7809bf3df3475bc6564
Taimoorkhokhar94 <taimoorkhokhar1994@gmail.com>
2021-08-02 00:34:49 +0500

    add GPTNEO and GPT Dialog support using openchat

--- server/grpc/example.py
+++ server/grpc/example.py
@@ -1,4 +1,8 @@
 
+import sys
+sys.path.append("..")
+from openchat.agents.dialogpt import DialoGPTAgent
+from openchat.agents.gptneo import GPTNeoAgent
 import requests
 import openai
 import json
@@ -14,11 +18,23 @@ def handle_message(sender, message):
             engine = 'curie-instruct-beta'
             context = 'Agent is a teacher expert in Web Development. You have skills in HTML And the problem your student is telling you about is I have been trying to center a div inside another div for over an hour now, I just can not do it, i need help with it..'
             rasa_model_name = '1'
+            dialog_gpt_model = 'dialogpt.medium'
+            gpt_neo_model = 'gptneo.medium'
             gpt3_agent = Gpt3Agent(engine, context, message)
             rasa_agent = RasaAgent(rasa_model_name, message)
+            dialog_gpt_agent = DialoGPTAgent(model=dialog_gpt_model, device="cpu")
+            gpt_neo_agent = GPTNeoAgent(model=gpt_neo_model, device="cpu")
+            dialog_gpt_response = dialog_gpt_agent.predict(message)
+            gpt_neo_response = gpt_neo_agent.predict(message)
             gpt3_response = gpt3_agent.invoke_api()
             rasa_response = rasa_agent.invoke()
-            return "Respond to message from " + sender + " | " + message + "\n\n" + "GPT3 Response" + " | " + gpt3_response + "\n\n" + "Rasa Response" + " | " + rasa_response
+            
+            return f'''Respond to message from  {sender} | {message} \n\n 
+                        GPT3 Response | {gpt3_response} \n\n
+                        Rasa Response | {rasa_response} \n\n 
+                        Dialog GPT Response | {dialog_gpt_response} \n\n
+                        GPT NEO Response | {gpt_neo_response}
+                    '''
         except Exception as err:
             return "Exception: " + str(err)
 
