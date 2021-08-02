86ce9519b9c4d3386e9b6c7036c8bf711e978887
Taimoorkhokhar94 <taimoorkhokhar1994@gmail.com>
2021-08-02 15:36:24 +0500

    import gpt3 and rasa classes from openchat

--- server/grpc/example.py
+++ server/grpc/example.py
@@ -5,9 +5,8 @@ parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 from openchat.agents.dialogpt import DialoGPTAgent
 from openchat.agents.gptneo import GPTNeoAgent
-import requests
-import openai
-import json
+from openchat.agents.gpt3 import GPT3Agent
+from openchat.agents.rasa import RasaAgent
 
 
 def handle_message(sender, message):
@@ -19,9 +18,9 @@ def handle_message(sender, message):
             engine = 'curie-instruct-beta'
             context = 'Agent is a teacher expert in Web Development. You have skills in HTML And the problem your student is telling you about is I have been trying to center a div inside another div for over an hour now, I just can not do it, i need help with it..'
             rasa_model_name = '1'
-            dialog_gpt_model = 'dialogpt.medium'
-            gpt_neo_model = 'gptneo.medium'
-            gpt3_agent = Gpt3Agent(engine, context, message)
+            dialog_gpt_model = 'dialogpt.small'
+            gpt_neo_model = 'gptneo.small'
+            gpt3_agent = GPT3Agent(engine, context, message)
             rasa_agent = RasaAgent(rasa_model_name, message)
             dialog_gpt_agent = DialoGPTAgent(model=dialog_gpt_model, device="cpu", maxlen=48)
             gpt_neo_agent = GPTNeoAgent(model=gpt_neo_model, device="cpu", maxlen=48)
@@ -40,79 +39,3 @@ def handle_message(sender, message):
             return "Exception: " + str(err)
 
 
-class Gpt3Agent():
-    engine_name = ''
-    chat_context = ''
-    question = ''
-    agent_response = ''
-    
-    def __init__(self, engine, context, question):
-        self.engine_name = engine
-        self.question = question
-        self.chat_context = context
-
-
-    def invoke(self):
-        prompt   = f'{self.chat_context}\nQ: {self.question}\nAgent:'
-        openai.organization = os.getenv("OPENAI_ORG_ID")
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        completion = openai.Completion()
-        response = completion.create(
-                                        prompt=prompt, engine=self.engine_name, stop=['\nQ:'], temperature=0.9,
-                                        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
-                                        max_tokens=80
-                                    )
-        self.agent_response = response.choices[0].text.strip()
-        return self.agent_response
-    
-    
-    def invoke_api(self):
-        url = f"https://api.openai.com/v1/engines/{self.engine_name}/completions"
-        payload = "{\n\t \"prompt\": \"" + self.chat_context + "\\nQ:" + self.question + "\\nAgent:\",\n\t  \"max_tokens\": 25,\n\t  \"temperature\": 1,\n\t  \"top_p\": 1,\n\t  \"n\": 1,\n\t  \"stream\": false,\n\t  \"logprobs\": null,\n\t  \"stop\": \"\\n\"\n}"        
-        api_key = os.getenv("OPENAI_API_KEY")
-        headers = {
-            'content-type': "application/json",
-            'authorization': f"Bearer {api_key}"
-            }
-        response = requests.request("POST", url, data=payload, headers=headers)
-        response_dic = json.loads(response.text)
-        self.agent_response = response_dic['choices'][0]['text']
-        return self.agent_response
-
-
-from rasa.core.agent import Agent
-from rasa.utils.endpoints import EndpointConfig
-from rasa import train
-import asyncio
-
-
-class RasaAgent():
-    model_name = ''
-    response = ''
-    question = ''
-    
-    
-    def __init__(self, model, question):
-        self.model_name = model
-        self.question = question
-        
-    
-    def invoke(self):
-        action_endpoint = "http://localhost:5055/webhook"
-        output_path	= "rasa/models/"
-        full_modal_path = output_path+self.model_name+".tar.gz"
-        agent = Agent.load(full_modal_path, action_endpoint=EndpointConfig(action_endpoint))
-        if agent.is_ready():
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            loop = asyncio.get_event_loop()
-            self.response = loop.run_until_complete(agent.handle_text(self.question))
-            agent_response_data = agent.tracker_store.retrieve_full_tracker("default")._latest_message_data()
-            intent = agent_response_data.get('intent',{})
-            confidence = intent.get('confidence', 0)            
-            if confidence >= 0.20:
-                return self.response[0].get("text")
-            else:
-                return "Im sorry can you elaborate?"
-        else:
-            return "No Agent Found"
