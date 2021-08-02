82c7472f28e45542b335a445ef5c75d997656d02
Taimoorkhokhar94 <taimoorkhokhar1994@gmail.com>
2021-08-03 03:01:45 +0500

    remove redundant imports and update gpt3 context story

--- server/grpc/example.py
+++ server/grpc/example.py
@@ -3,8 +3,6 @@ import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
-from openchat.agents.dialogpt import DialoGPTAgent
-from openchat.agents.gptneo import GPTNeoAgent
 from openchat.agents.gpt3 import GPT3Agent
 from openchat.agents.rasa import RasaAgent
 from openchat import OpenChat
@@ -15,30 +13,34 @@ def handle_message(sender, message):
     print(sender)
     print(message)
     if sender and message:
-    # try:
-        engine = 'curie-instruct-beta'
-        context = 'Agent is a teacher expert in Web Development. You have skills in HTML And the problem your student is telling you about is I have been trying to center a div inside another div for over an hour now, I just can not do it, i need help with it..'
-        rasa_model_name = '1'
-        dialog_gpt_model = 'dialogpt.small'
-        gpt_neo_model = 'gptneo.small'
-        gpt3_agent = GPT3Agent(engine, context, message)
-        rasa_agent = RasaAgent(rasa_model_name, message)
-        dialog_gpt_agent = OpenChat(model=dialog_gpt_model, device="cpu")
-        gpt_neo_agent = OpenChat(model=gpt_neo_model, device="cpu")
-        dialog_gpt_env = dialog_gpt_agent.create_environment_by_name(dialog_gpt_agent.environment)
-        gpt_neo_env = gpt_neo_agent.create_environment_by_name(gpt_neo_agent.environment)
-        dialog_gpt_response = dialog_gpt_env.environment.start(dialog_gpt_agent.agent, user_message=message)
-        gpt_neo_response = gpt_neo_env.environment.start(gpt_neo_agent.agent, user_message=message)
-        gpt3_response = gpt3_agent.invoke_api()
-        rasa_response = rasa_agent.invoke()
-        
-        return f'''Respond to message from  {sender} | {message} \n\n 
-                    GPT3 Response | {gpt3_response} \n\n
-                    Rasa Response | {rasa_response} \n\n 
-                    Dialog GPT Response | {dialog_gpt_response} \n\n
-                    GPT NEO Response | {gpt_neo_response}
-                '''
-    # except Exception as err:
-    #     return "Exception: " + str(err)
+        try:
+            engine = 'curie-instruct-beta'
+            context =   '''
+                            user is a student of einstein. einstein is a german born theoretical physicist, 
+                            widely acknowledged to be one of the greatest physicists of all time. Einstein is known for developing the theory
+                            of relativity, but he has also made important contributions to the development of theory of quantum mechanics
+                        '''
+            rasa_model_name = '1'
+            dialog_gpt_model = 'dialogpt.small'
+            gpt_neo_model = 'gptneo.small'
+            gpt3_agent = GPT3Agent(engine, context, message)
+            rasa_agent = RasaAgent(rasa_model_name, message)
+            dialog_gpt_agent = OpenChat(model=dialog_gpt_model, device="cpu")
+            gpt_neo_agent = OpenChat(model=gpt_neo_model, device="cpu")
+            dialog_gpt_env = dialog_gpt_agent.create_environment_by_name(dialog_gpt_agent.environment)
+            gpt_neo_env = gpt_neo_agent.create_environment_by_name(gpt_neo_agent.environment)
+            dialog_gpt_response = dialog_gpt_env.environment.start(dialog_gpt_agent.agent, user_message=message)
+            gpt_neo_response = gpt_neo_env.environment.start(gpt_neo_agent.agent, user_message=message)
+            gpt3_response = gpt3_agent.invoke_api()
+            rasa_response = rasa_agent.invoke()
+            
+            return f'''Respond to message from  {sender} | {message} \n\n 
+                        GPT3 Response | {gpt3_response} \n\n
+                        Rasa Response | {rasa_response} \n\n 
+                        Dialog GPT Response | {dialog_gpt_response} \n\n
+                        GPT NEO Response | {gpt_neo_response}
+                    '''
+        except Exception as err:
+            return "Exception: " + str(err)
 
 
