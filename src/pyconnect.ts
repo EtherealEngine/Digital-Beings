434361758a279ac5a709d5c1497eafe46aaa2825
Taimoorkhokhar94 <taimoorkhokhar1994@gmail.com>
2021-08-03 20:03:25 +0500

    change grpc client port

--- src/pyconnect.ts
+++ src/pyconnect.ts
@@ -3,7 +3,7 @@ const path = require('path');
 const grpc = require('grpc');
 
 const proto = grpc.load(`${__dirname}/../server/grpc/example.proto`)
-const PORT = 50052
+const PORT = 50051
 const IP = 'localhost'
 
 class PyConnect {
