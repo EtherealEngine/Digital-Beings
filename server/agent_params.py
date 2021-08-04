aaa76a8be755aaed31351f91266470c5bb530fab
Taimoorkhokhar94 <taimoorkhokhar1994@gmail.com>
2021-08-05 02:23:17 +0500

    move agent_params.py file in server dir

---
+++ server/agent_params.py
@@ -0,0 +1,29 @@
+ALL_AGENTS_LIST = [
+                    'blender.small',
+                    'blender_large',
+                    'blender.xlarge',
+                    'blender.xxlarge',
+                    'unlikelihood.convai2.vocab.alpha.1e-1',
+                    'dialogpt.small',
+                    'dialogpt.medium',
+                    'dialogpt.large',
+                    'dodecathlon.all_tasks_mt',
+                    'dodecathlon.convai2',
+                    'dodecathlon.daily_dialog',
+                    'dodecathlon.wizard_of_wikipedia',
+                    'gptneo.large',
+                    'gptneo.small',
+                    'gptneo.xlarge',
+                    'reddit.xlarge',
+                    'reddit.xxlarge',
+                    'wizard_of_wikipedia.end2end_generator'
+                  ]
+SELECTED_AGENTS = [
+                    'dialogpt.small',
+                    'gptneo.small'    
+                  ]
+DEVICE = 'cpu'  # cpu or gpu
+ENVIRONMENT = 'custom' 
+GPT3_ENGINE = 'curie-instruct-beta'
+CONTEXT = 'user is a student of einstein. einstein is a german born theoretical physicist, widely acknowledged to be one of the greatest physicists of all time. Einstein is known for developing the theory of relativity, but he has also made important contributions to the development of theory of quantum mechanics'
+RASA_MODEL_NAME = '1' # default
