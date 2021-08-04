19d5f4554ff0600d8a324e50efb4c65f16cb101b
Taimoorkhokhar94 <taimoorkhokhar1994@gmail.com>
2021-08-05 01:52:22 +0500

    add parameters for agent initialization

---
+++ digital_being_params.py
@@ -0,0 +1,26 @@
+ALL_AGENTS_LIST = [
+                    'blender.small',
+                    'blender_large',
+                    'blender.xlarge',
+                    'blender.xxlarge',
+                    'unlikelihood.convai2.vocab.alpha.1e-1',
+                    'dialogpt.small',
+                    'dialogpt.medium',
+                    'dialogpt.large',
+                    'dodecathlon.all_tasks_mt',
+                    'dodecathlon.convai2',
+                    'dodecathlon.daily_dialog',
+                    'dodecathlon.wizard_of_wikipedia',
+                    'gptneo.large',
+                    'gptneo.small',
+                    'gptneo.xlarge',
+                    'reddit.xlarge',
+                    'reddit.xxlarge',
+                    'wizard_of_wikipedia.end2end_generator'
+                  ]
+SELECTED_AGENTS = [
+                    'dialogpt.small',
+                    'gptneo.small'    
+                  ]
+DEVICE = 'cpu'  # cpu or gpu
+ENVIRONMENT = 'custom' 
