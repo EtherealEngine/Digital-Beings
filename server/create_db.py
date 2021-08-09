a06eab5cc1334ffc6c49e733b647f654fa080563
Taimoor Khokhar <taimoorkhokhar1994@gmail.com>
2021-08-09 17:56:33 +0500

    create a sqlite db and insert default agents

---
+++ server/create_db.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+from sqlite3 import Error
+import agent_params
+
+def sql_connection():
+
+    try:
+
+        con = sqlite3.connect('digitalbeing.db')
+
+        return con
+
+    except Error:
+
+        print(Error)
+
+def sql_table(con):
+
+    cursorObj = con.cursor()
+
+    cursorObj.execute("CREATE TABLE Agents(id integer PRIMARY KEY AUTOINCREMENT, name text, question text, reaponse text)")
+
+    # Insert default selected agents
+    entities = res = [(agent,) for agent in agent_params.SELECTED_AGENTS]
+
+    if len(entities) > 1:
+        cursorObj.executemany('INSERT INTO Agents(name) VALUES(?)', entities)
+    else:
+        cursorObj.execute('INSERT INTO Agents(name) VALUES(?)', entities)
+
+    con.commit()
+
+con = sql_connection()
+sql_table(con)
