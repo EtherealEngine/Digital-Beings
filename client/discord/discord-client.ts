b17af5513de9c72f6b5f87a5e75ec3382e6fcd7f
alextitonis <alexandrostitonis@gmail.com>
2021-08-17 11:52:19 +0300

    Fixed an error (discord client missing Intents)

--- client/discord/discord-client.ts
+++ client/discord/discord-client.ts
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const {Util} = require('discord.js')
+const {Util, Intents} = require('discord.js')
 const config = require("./config.json");
 const util = require('./util.ts')
 console.log("util ==> ", util)
@@ -9,7 +9,8 @@ const DISCORD_API_TOKEN = process.env.DISCORD_API_TOKEN;
 const createDiscordClient = (messageResponseHandler) => {
     
     if (!DISCORD_API_TOKEN) return console.warn("No API token for Discord bot, skipping");
-    const client = new Discord.Client();
+	//The discord client was crashing with intents, so i've added some Core Intents
+    const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
     // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
     client.config = config;
     client.helpFields = util.helpFields;
