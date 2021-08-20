a9297ad2948faed90c10ba56ce96de5e464ba65c
sifanduronto <sifan@sifans-MacBook-Air.local>
2021-08-20 12:58:09 +0600

    only replying to the content script added

--- server/agents/echo/echo.ts
+++ server/agents/echo/echo.ts
@@ -16,23 +16,14 @@ const createEcho = (messageResponseHandler) => {
             if (message.author.bot)
                 return; // Skip own messages
             if (message.channel.type === 'text') {
-                if (message.mentions.has(client.user)) {
                     message.channel.startTyping();
-                    let removeMentions = message.content.split(" ").slice(1).join(" ")
-                    message.content = removeMentions.replace(/<@[!&]?\d+>/, '[mention]');
-                    await messageResponseHandler(message.author.username, message.content, (response) => {
-                        message.reply(response.value)
-                        message.channel.stopTyping();
-                    });
-                } else {
                     message.reply(message.content);
-                }
+                    message.channel.stopTyping()
+
             } else if (message.channel.type === 'dm') {
                 message.channel.startTyping();
-                await messageResponseHandler(message.author.username, message.content, (response) => {
-                    message.author.send(response.value)
-                    message.channel.stopTyping();
-                });
+                message.reply(message.content);
+                message.channel.stopTyping()
             }
         });
     });
