873a3f3d18ccd908418b507df5058328a303145d
Taimoor Khokhar <taimoorkhokhar1994@gmail.com>
2021-08-24 02:44:42 +0500

    Resolve grpc Exception

--- src/pyconnect.ts
+++ src/pyconnect.ts
@@ -3,7 +3,7 @@ const path = require('path');
 const grpc = require('grpc');
 
 const proto = grpc.load(`${__dirname}/../server/grpc/example.proto`)
-const PORT = 50061
+const PORT = 50051
 const IP = 'localhost'
 
 class PyConnect {
