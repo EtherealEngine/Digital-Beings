00c952f4dce602aabbbe85fe43779a24cbd2d41d
alextitonis <alexandrostitonis@gmail.com>
2021-09-05 13:01:42 +0300

    fixed some issues with the pull, updated the readme for the TTS model

--- client/xr/xrengine-client.ts
+++ client/xr/xrengine-client.ts
@@ -121,65 +121,7 @@ class XREngineBot {
     }
     async requestAllWorldMetadata() {
         await this.requestWorldMetadata(Number.MAX_SAFE_INTEGER)
-    }  async goTo(landmark: string) { 
-        if (landmark === undefined || landmark === '') return
-
-        await this.sendMessage('/goTo ' + landmark)
-    }
-    async playEmote(emote: string) {
-        if (emote === undefined || emote === '') return
-
-        await this.sendMessage('/emote ' + emote)
-    }
-    async playFaceExpression(types: string[], perc: string[], time: string) {
-        if (types === undefined || types.length <= 0) return
-        if (types.length !== perc.length) return
-
-        var message: string = '/face '
-        for(var i = 0; i < types.length; i++) 
-            message += types[i] + ' ' + perc[i] + ' '
-        message += time
-
-        await this.sendMessage(message)
-    }
-    async getPosition(player: string) {
-        if (player === undefined || player === '') return
-
-        await this.sendMessage('/getPosition ' + player)
-    }
-    async getRotation(player: string) {
-        if (player === undefined || player === '') return
-
-        await this.sendMessage('/getRotation ' + player)
-    }
-    async getScale(player: string) {
-        if (player === undefined || player === '') return
-
-        await this.sendAudio('/getScale ' + player)
-    }
-    async getTransform(player: string) {
-        if (player === undefined || player === '') return
-
-        await this.sendMessage('getTransform ' + player)
-    }
-    async subscribeToChatSystem(system: string) {
-        if (system === undefined || system === '') return
-
-        await this.sendMessage('/subscribe ' + system)
-    }
-    async unsubscribeFromChatSystem(system: string) {
-        if (system === undefined || system === '') return
-
-        await this.sendMessage('/unsubscribe ' + system)
-    }
-    async getSubscribedChatSystems() {
-        await this.sendMessage('/getSubscribed')
-    }
-    async follow(player: string) {
-        if (player === undefined || player === '') return
-
-        await this.sendMessage('/follow ' + player)
-    }
+    }  
     async requestPlayers() {
         await this.sendMessage('/listAllusers ')
     }
