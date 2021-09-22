835117fdb9b28e006eb04b7dc135e3bf9c7e6627
alextitonis <alexandrostitonis@gmail.com>
2021-09-22 16:38:43 +0300

    fixed the !agents command when there are no agents

--- client/discord/commands/agents.ts
+++ client/discord/commands/agents.ts
@@ -1,14 +1,16 @@
 exports.run = async (client, message, args) => {
     await client.messageResponseHandler(args, (res) => {
+        let str: string = ''
         Object.keys(res.response).map(function(key, index) {
             const label = res.response[key];
             const agent_name = key;
-            client.embed
-            .addFields({name: label , value: agent_name})
+            str += 'name: ' + label + ', value: ' + agent_name + '\n'
             
         });
-        message.channel.send(client.embed);
+        client.embed.description = str === '' ? 'not agents found' : str
+        message.channel.send(client.embed)
         client.embed.fields = [];  // clear previous responses
+        client.embed.description = ''
 
         message.channel.stopTyping();
     });
