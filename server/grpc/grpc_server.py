36dcc1c652e86626b2e8025a1873835da2243483
Mohamed Amine Ouertani <amineouertani123123@gmail.com>
2021-10-05 18:38:23 +0100

    Added error logging plus other fixes

--- server/grpc/grpc_server.py
+++ server/grpc/grpc_server.py
@@ -1,5 +1,6 @@
 import grpc
 from concurrent import futures
+import logging
 import time
 import os
 
@@ -9,6 +10,14 @@ import example_pb2_grpc
 
 # import the original example.py
 from example import DigitalBeing as DB
+
+logger = logging.getLogger('server_logger')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('grpc_server.log')
+fh.setLevel(logging.DEBUG)
+
+logger.addHandler(fh)
 
 # create a class to define the server functions, derived from
 # example_pb2_grpc.AgentServicer
@@ -21,13 +30,14 @@ class AgentServicer(example_pb2_grpc.AgentServicer):
     # example_pb2.Request
     def InitializeAgents(self, request, context):
         try:
-            response_obj = example_pb2.Response()
-            response_obj.response.update({"response":"Initialized all agents"})
-            return response_obj
+            response = example_pb2.Response()
+            response.response.update({"response":"Initialized all agents"})
+            return response
         except Exception as err:
+            logger.exception()
             DB.sendDiscordMessage('InitializeAgents exception:' + err)
             print('InitializeAgents exception:' + err)
-            return {"response":"Initialized all agents"}
+            return {"response":"Couldn't initialize all agents"}
 
 
     # example.handle_message is exposed here
@@ -37,13 +47,15 @@ class AgentServicer(example_pb2_grpc.AgentServicer):
         print('handle message')
         print(request)
         try:
-            response_obj = example_pb2.Response()
+            response = example_pb2.Response()
             agent_responses = self.digital_being.handle_message(**request.kwargs)
-            response_obj.response.update(agent_responses)
-            if (response_obj == None):
-                response_obj = { 'none': 'none' }
-            return response_obj
+            response.response.update(agent_responses)
+            if (response == None):
+                response = { 'none': 'none' }
+            return response
         except Exception as err:
+            print("exception")
+            logger.exception()
             DB.sendDiscordMessage('HandleMessage exception: ' + err)
             print('HandleMessage exception: ' + err)
             return { 'none': 'none' }        
@@ -58,6 +70,7 @@ class AgentServicer(example_pb2_grpc.AgentServicer):
                 response_obj = { 'key': 'none', 'value': 'name' }
             return response_obj
         except Exception as err:
+            logger.exception()
             DB.sendDiscordMessage('GetAgents exception: ' + err)
             print('GetAgents exception: ' + err)
             return { 'key': 'none', 'value': 'name' }
@@ -73,6 +86,7 @@ class AgentServicer(example_pb2_grpc.AgentServicer):
                 response_obj = {'name': 'none', 'context': 'none'}
             return response_obj
         except Exception as err:
+            logger.exception()
             DB.sendDiscordMessage('SetAgentFields exception: ' + err)
             print('SetAgentFields exception: ' + err)
             return {'name': 'none', 'context': 'none'}
@@ -88,26 +102,38 @@ class AgentServicer(example_pb2_grpc.AgentServicer):
                 response_obj = { 'key': 'none', 'value': 'none' }
             return response_obj
         except Exception as err:
+            logger.exception()
             DB.sendDiscordMessage('InvokeSoloAgent exception: ' + err)
             print('InvokeSoloAgent exception: ' + err)
             return { 'key': 'none', 'value': 'none' }
        
-# create a gRPC server
-server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
-# use the generated function `add_AgentServicer_to_server`
-# to add the defined class to the server
+try:
+    # create a gRPC server
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
-example_pb2_grpc.add_AgentServicer_to_server(
-    AgentServicer(), server)
+    # use the generated function `add_AgentServicer_to_server`
+    # to add the defined class to the server
 
-# listen on port 7777
-print('Starting server. Listening on port ' + os.getenv('GRPC_SERVER_PORT') + '.')
-print('started server1')
-server.add_insecure_port('[::]:' + os.getenv('GRPC_SERVER_PORT'))
-print('started server2')
-server.start()
-print('started server3')
+    example_pb2_grpc.add_AgentServicer_to_server(
+        AgentServicer(), server)
+
+    port = os.getenv('GRPC_SERVER_PORT') 
+
+    if (port == None):
+        print("env for grpc server port is not configured")
+        exit()
+
+    # listen on port 7777
+    print('Starting server. Listening on port ' + port + '.')
+    print('started server1')
+    server.add_insecure_port('[::]:' + port)
+    print('started server2')
+    server.start()
+    print('started server3')
+
+except Exception as err:
+    logger.exception("launching server")
 
 # since server.start() will not block,
 # a sleep-loop is added to keep alive
