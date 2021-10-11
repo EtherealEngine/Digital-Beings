211d43a79d721a83011a0500aa46a593b19addec
alextitonis <alexandrostitonis@gmail.com>
2021-10-11 12:10:37 +0300

    createda book file reader

---
+++ client/website-reader/website-reader.ts
@@ -0,0 +1,84 @@
+import { detectOsOption } from "../utils";
+const browserLauncher = require('../../src/browser-launcher')
+import * as fs from 'fs';
+
+let browser;
+let page;
+
+export const createWebsiteReader = async (bookUrl: string, maxPage) => {
+    const options = {
+        headless: true,
+        ignoreHTTPSErrors: true,
+        args: [
+            '--disable-web-security=1',
+            "--autoplay-policy=no-user-gesture-required",
+        ],
+        ignoreDefaultArgs: ['--mute-audio', '--mute-video'],
+        ...detectOsOption()
+    };
+    
+    browser = await browserLauncher.browser(options);
+    page = await browser.newPage()
+    page.setViewport({ width: 0, height: 0 })
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36')
+    await navigate(bookUrl);
+    await delay(5000);
+
+    for(let i = 1; i < maxPage; i++) {
+        const text = await page.evaluate(() => {
+            const el = document.getElementById('demo');
+            return el.innerHTML
+        })
+    
+        const data = getText(text);
+        fs.writeFileSync('page' + i + '.txt', data);
+        navigate(bookUrl.substring(0, bookUrl.length - 1) + (i + 1))
+        await delay(5000);
+    }
+};
+
+const getText = (text: string) => {
+    const lines: string[] = text.split('\n')
+    let _res: string = ''
+    for(let i = 0; i < lines.length;i++) {
+        if (!lines[i].toLowerCase().includes('<p>') && !lines[i].toLowerCase().includes('</p>')) {
+            //
+        }
+        else {
+            _res = lines[i].split('<p>').join('')
+            _res = _res.split('</p>').join('\n');
+            _res = _res.replace(/<\/?[^>]+(>|$)/g, "");
+        }
+    }
+    return _res.trim();
+}
+
+const navigate = async(url, searchParams: { [param: string]: string } = undefined) => {
+    let parsedUrl = new URL(url.includes('https') ? url : `https://${url}`);
+    if (searchParams !== undefined) {
+        for(let x in searchParams) {
+            parsedUrl.searchParams.set(x, searchParams[x])
+        }
+    }
+    const context = browser.defaultBrowserContext();
+    context.overridePermissions(parsedUrl.origin, ['microphone', 'camera']);
+    console.log('navigating to: ' + parsedUrl)
+    await page.goto(parsedUrl, { waitUntil: 'domcontentloaded' });
+}
+
+let counter: number = 0
+const catchScreenshot = async() => {
+    counter++;
+    console.log('screenshot');
+    const path = 'screenshot' + counter + '.png'
+    await page.screenshot({ path })
+}
+
+const delay = async(timeout) => {
+    console.log(`Waiting for ${timeout} ms... `);
+    await waitForTimeout(timeout);
+}
+
+const waitForTimeout = async(timeout) => {
+    return await new Promise<void>(resolve => setTimeout(() => resolve(), timeout));
+}
--- src/initialize.ts
+++ src/initialize.ts
@@ -35,7 +35,7 @@ const pyConnect = require('./pyconnect');
                 args.response = await pyConnect.invoke(args)
                 callback(args.response);
             }
-            require("../client/discord/discord-client").createDiscordClient(messageResponseHandler);
+            //require("../client/discord/discord-client").createDiscordClient(messageResponseHandler);
             //require("../client/messenger/messenger-client").createMessengerClient(messageResponseHandler);
             //require('../client/telegram/telegram-client').createTelegramClient(messageResponseHandler);
             //require("../client/twilio/twilio-client").createTwilioClient(messageResponseHandler);
@@ -43,6 +43,7 @@ const pyConnect = require('./pyconnect');
             //require("../client/twitter/twitter-client").createTwitterClient(messageResponseHandler);
             //require("../client/xr/xrengine-client").createXREngineClient(messageResponseHandler);
             //require("../client/zoom/zoom-client").createZoomClient(messageResponseHandler);
+            require("../client/website-reader/website-reader.ts").createWebsiteReader('https://booksvooks.com/scrolablehtml/pale-blue-dot-a-vision-of-the-human-future-in-space-pdf.html?page=1', 210);
         }
     ); 
 })();
