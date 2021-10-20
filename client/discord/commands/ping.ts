1acba704d4dd512b54b3cce124ecad3926268695
alextoti <alexandrostitonis@gmail.com>
2021-10-20 12:51:48 +0300

    made the bot reply to messages rather than just ping

--- client/discord/commands/ping.ts
+++ client/discord/commands/ping.ts
@@ -1,6 +1,7 @@
 import { getRandomEmptyResponse } from "../../utils";
 import { addMessageToHistory, onMessageResponseUpdated } from "../chatHistory";
 import { replacePlaceholders } from "../util";
+const inlinereply = require('discord-reply');
 
 export async function run (client, message, args, author, addPing, channel) {
     if ( args.grpc_args.message === undefined ||  args.grpc_args.message === '' || args.grpc_args.message.replace(/\s/g, '').length === 0) {
@@ -26,15 +27,14 @@ export async function run (client, message, args, author, addPing, channel) {
             Object.keys(response.response).map(function(key, index) {
                 console.log('response: ' + response.response[key])
                 if (response.response[key] !== undefined && response.response[key].length <= 2000 && response.response[key].length > 0) {
+                    let text = replacePlaceholders(response.response[key])
                     if (addPing) {
-                        const text = '<@!' + author + '> ' + replacePlaceholders(response.response[key])
-                        message.channel.send(text).then(async function (msg) {
+                        message.lineReply(text).then(async function (msg) {
                             onMessageResponseUpdated(channel, message.id, msg.id)
                             addMessageToHistory(channel, msg.id, process.env.BOT_NAME, text)
                         }).catch(console.error)
 
                     }  else {
-                        let text = replacePlaceholders(response.response[key])
                         while (text === undefined || text === '' || text.replace(/\s/g, '').length === 0) text = getRandomEmptyResponse()
                         console.log('response1: ' + text)
                         message.channel.send(text).then(async function (msg) {
@@ -44,18 +44,15 @@ export async function run (client, message, args, author, addPing, channel) {
                     }
                 }
                 else if (response.response[key].length >= 2000) {
-                    let text: string = ''
+                    let text: string = replacePlaceholders(response.response[key])
                     if (addPing) {
-                        text = '<@!' + author + '> ' + replacePlaceholders(response.response[key])
-                        message.channel.send(text).then(async function (msg) {
+                        message.lineReply(text).then(async function (msg) {
                             onMessageResponseUpdated(channel, message.id, msg.id)
                             addMessageToHistory(channel, msg.id, process.env.BOT_NAME, text)
                         })
                     } else {
-                        text = replacePlaceholders(response.response[key])
                         while (text === undefined || text === '' || text.replace(/\s/g, '').length === 0) text = getRandomEmptyResponse()
                         console.log('response2: ' + text)
-                        
                     }
                     if (text.length > 0) {
                         message.channel.send(text, { split: true }).then(async function (msg) {
@@ -68,14 +65,13 @@ export async function run (client, message, args, author, addPing, channel) {
                     const emptyResponse = getRandomEmptyResponse()
                     console.log('sending empty response: ' + emptyResponse)
                     if (emptyResponse !== undefined && emptyResponse !== '' && emptyResponse.replace(/\s/g, '').length !== 0) {
+                        let text = emptyResponse
                         if (addPing) {
-                            const text = '<@!' + author + '> ' + emptyResponse
-                            message.channel.send(text).then(async function (msg) {
+                            message.lineReply(text).then(async function (msg) {
                                 onMessageResponseUpdated(channel, message.id, msg.id)
                                 addMessageToHistory(channel, msg.id, process.env.BOT_NAME, text)
                             }).catch(console.error)
                         } else {
-                            let text = emptyResponse
                             while (text === undefined || text === '' || text.replace(/\s/g, '').length === 0) text = getRandomEmptyResponse()
                             console.log('response4: ' + text)
                             message.channel.send(text).then(async function (msg) {
