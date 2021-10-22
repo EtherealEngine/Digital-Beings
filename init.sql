667e0b95f1c65f77ee5bccfceed310f9c926f0fc
alextitonis <45359358+alextitonis@users.noreply.github.com>
2021-10-22 13:05:57 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS chat_history(client_name text, chat_id text, message_
 CREATE TABLE IF NOT EXISTS blocked_users(user_id varchar(255), client varchar(25));
 DROP TABLE IF EXISTS chat_filter;
 CREATE TABLE chat_filter(half int, max int);
-INSERT INTO TABLE chat_filter(half, max) VALUES(5, 10);
+INSERT INTO chat_filter(half, max) VALUES(5, 10);
 CREATE TABLE IF NOT EXISTS bad_words(word varchar(255), rating int);
