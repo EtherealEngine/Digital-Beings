f80b049a5950edc3f3a2a7b1c617c6762135cd50
alextoti <alexandrostitonis@gmail.com>
2021-10-25 15:51:21 +0300

    Update keywordsManager.py

--- server/grpc/keywordsManager.py
+++ server/grpc/keywordsManager.py
@@ -10,14 +10,6 @@ class keywordsManager:
         for i in self.keywords:
             if i['agent'] == agent:
                 if i['word'] in text.lower():
-                    if i['replacementRules'] != None and len(i['replacementRules']) > 0:
-                        for x in i['replacementRules'].split(','):
-                            parts = x.split(':')
-                            if (parts == None or len(parts) < 1):
-                                continue
-
-                            text = text.replace(re.compile(parts[0], re.IGNORECASE), '' if len(parts) > 1 else parts[1])
-                    
                     return text, int(i['count'])
         
         for i in self.keywords:
