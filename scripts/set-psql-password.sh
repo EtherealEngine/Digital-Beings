062601f5bd25a916c77bc046600da7642f0ae971
Grid <vic.6r1d@gmail.com>
2021-12-17 21:37:14 +0300

    Removes Supervisor from Docker. Again.

--- scripts/set-psql-password.sh
+++ scripts/set-psql-password.sh
@@ -1,3 +1,4 @@
-/usr/bin/supervisord &
-sleep 10
+#!/bin/bash
+# Had Supervisor running for no reason, adding sleep, since IDK why it was here
+sleep 1
 su postgres -c "psql -c \"ALTER USER postgres WITH PASSWORD 'admin123456'\" -d template1"
