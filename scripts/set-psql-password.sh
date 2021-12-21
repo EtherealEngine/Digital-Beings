90313f82640efa29a931ad5204f878dbcd2ed686
Grid <vic.6r1d@gmail.com>
2021-12-21 05:40:23 +0300

    Deprecate unused code

--- scripts/set-psql-password.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Had Supervisor running for no reason, adding sleep, since IDK why it was here
-sleep 1
-su postgres -c "psql -c \"ALTER USER postgres WITH PASSWORD 'admin123456'\" -d template1"
